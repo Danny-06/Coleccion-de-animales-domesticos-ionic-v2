d70fe85f1d9943d9a64b6efe4965edf4fbc92e7a
Danny06 <daniel.elbelaizi.alu@iescampanillas.com>
2022-03-01 19:23:08 +0100

    commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'coleccion-de-animales-domesticos',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
